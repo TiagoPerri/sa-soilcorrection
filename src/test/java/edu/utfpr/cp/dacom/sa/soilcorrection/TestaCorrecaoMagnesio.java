49d1b68080999b443ad40763bdc3fffbf0aa2c16
TiagoPerri <tiagocnsr@gmail.com>
2021-07-05 20:22:12 -0300

    Add @test em TestaCorrecaoMagnesio

--- src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoMagnesio.java
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoMagnesio.java
@@ -13,4 +13,21 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class TestaCorrecaoMagnesio {
     
+    @Test
+    public void testaCorrecaoMagnesio() {
+        double somaKCaMg = new CorrecaoMagnesio().somaKCaMg(0.11, 5.72, 1.59);
+        assertEquals(13.84, new CorrecaoMagnesio().CalcCorrecao(1.75,somaKCaMg,5.31));
+    }
+    
+    @Test
+    public void testaCorrecaoMagnesio() {
+        double somaKCaMg = new CorrecaoMagnesio().somaKCaMg(0.11, 5.60, 1.47);
+        assertEquals(13.72, new CorrecaoMagnesio().CalcCorrecao(1.63,somaKCaMg,5.19));
+    }
+    
+    @Test
+    public void testaCorrecaoMagnesio() {
+        double somaKCaMg = new CorrecaoMagnesio().somaKCaMg(0.01, 5.62, 1.49);
+        assertEquals(13.74, new CorrecaoMagnesio().CalcCorrecao(1.65,somaKCaMg,5.21));
+    }
 }
